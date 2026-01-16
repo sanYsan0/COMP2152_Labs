70aea1533a41b649db0310cd91341b08c7b6fbef
sanyoung <101503234@georgebrown.ca>
2026-01-16 09:51:13 -0500

    add lab1

---
+++ Week01/lab1.py
@@ -0,0 +1,11 @@
+# Sample Coding Questions 01 Week 01
+# Sanyoung Yoon
+
+# Question 2: Defining an array (list)
+my_array = [1, 4, 7, 9]
+
+# Question 3: Order of Operations
+a = 1
+b = 1
+c = 1
+d = 1
